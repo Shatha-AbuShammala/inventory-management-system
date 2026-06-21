5dddb47279b49ef430b717e4940617c83e235e5d
Shatha-AbuShammala <shadamhr@gmail.com>
2026-06-22 02:09:02 +0300

    Update inventory urls

--- inventory_system/inventory/urls.py
+++ inventory_system/inventory/urls.py
@@ -12,9 +12,4 @@ urlpatterns = [
     path('ajax/save/<int:pk>/', views.product_save_ajax, name='product_save_edit'),
     path('ajax/delete/<int:pk>/', views.product_delete_ajax, name='product_delete'),
 
-    # # Stock Movement (Stage 7)
-    # path('stock/', views.stock_movement_page, name='stock_movement_page'),
-    # path('stock/ajax/form/', views.stock_form_ajax, name='stock_form_ajax'),
-    # path('stock/ajax/save/', views.stock_save_ajax, name='stock_save_ajax'),
-    # path('stock/ajax/history/', views.stock_history_ajax, name='stock_history_ajax'),
 ]
